013904c629fb01b82fd42e5edeb768e9c8538f49
hongwan <xmaaaa66@gmail.com>
2022-08-23 08:00:47 +0800

    git

--- src/main/java/com/xm/Test.java
+++ src/main/java/com/xm/Test.java
@@ -8,75 +8,13 @@ import java.util.Random;
  * @date 2021/6/9
  */
 public class Test {
-    private static final Random random = new Random();
 
 
     public static void main(String[] args) {
         int[] nums = new int[]{2,3,3,2,2,2,1,1};
-        wiggleSort(nums);
         System.out.println(Arrays.toString(nums));
     }
 
 
-    public static void wiggleSort(int[] nums) {
-        int n = nums.length;
-        int x = (n + 1) / 2;
-        int mid = x - 1;
-        int target = findKthLargest(nums, mid);
-        for (int k = 0, i = 0, j = n - 1; k <= j; k++) {
-            if (nums[k] > target) {
-                swap(nums, k, j--);
-            }
-            if (nums[k] < target) {
-                swap(nums, k, i++);
-            }
-        }
-        int[] arr = nums.clone();
-        for (int i = 0, j = mid, k = n - 1; i < n; i += 2, j--, k--) {
-            nums[i] = arr[j];
-            if (i + 1 < n) {
-                nums[i + 1] = arr[k];
-            }
-        }
-    }
 
-    public static int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, k);
-    }
-
-    public static int quickSelect(int[] a, int l, int r, int index) {
-        int q = randomPartition(a, l, r);
-        if (q == index) {
-            return a[q];
-        } else {
-            return q < index ? quickSelect(a, q + 1, r, index) : quickSelect(a, l, q - 1, index);
-        }
-    }
-
-    public static int randomPartition(int[] a, int l, int r) {
-        int i = random.nextInt(r - l + 1) + l;
-        swap(a, i, r);
-        return partition(a, l, r);
-    }
-
-    public static int partition(int[] a, int l, int r) {
-        int x = a[r], i = l, j =r;
-        while (i < j) {
-            while (i < j && a[i] <= x) {
-                i++;
-            }
-            while (i < j && a[j] >= x) {
-                j--;
-            }
-            swap(a, i, j);
-        }
-        swap(a, i, r);
-        return i;
-    }
-
-    public static void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
 }
--- src/main/java/com/xm/sort/QuickSort.java
+++ src/main/java/com/xm/sort/QuickSort.java
@@ -1,6 +1,7 @@
 package com.xm.sort;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * 快速排序
@@ -9,35 +10,89 @@ import java.util.Arrays;
  * @date 2022/8/19
  */
 public class QuickSort {
+    private static final Random random = new Random();
 
 
     public static void main(String[] args) {
         int[] arr = new int[]{5, 4, 3, 52, 6, 7};
         quickSort(arr, 0, arr.length - 1);
+        int quickSelect = quickSelect(arr, 0, arr.length - 1, 6);
         System.out.println(Arrays.toString(arr));
+        System.out.println(quickSelect);
     }
 
+    /**
+     * 快速排序
+     *
+     * @param arr
+     * @param start
+     * @param end
+     */
     private static void quickSort(int[] arr, int start, int end) {
         if (start >= end) {
             return;
         }
-        int left = start;
-        int right = end;
+        int partition = randomPartition(arr, start, end);
+        quickSort(arr, start, partition - 1);
+        quickSort(arr, partition + 1, end);
+    }
+
+
+    /**
+     * 快速选择
+     *
+     * @param a
+     * @param start
+     * @param end
+     * @param index
+     * @return
+     */
+    public static int quickSelect(int[] a, int start, int end, int index) {
+        if (start > index || end < index) {
+            return -1;
+        }
+        int q = randomPartition(a, start, end);
+        if (q == index) {
+            return a[q];
+        } else {
+            return q < index ? quickSelect(a, q + 1, end, index) : quickSelect(a, start, q - 1, index);
+        }
+    }
+
+    /**
+     * 此处为了更随机，从数组中随便取一位数进行交换
+     *
+     * @param a
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int randomPartition(int[] a, int start, int end) {
+        int i = random.nextInt(end - start + 1) + start;
+        swap(a, i, start);
+        return partition(a, start, end);
+    }
+
+    public static int partition(int[] a, int start, int end) {
+        int left = start, right = end;
         while (left < right) {
-            while (left < right && arr[right] >= arr[start]) {
+            // 如果被作为比较值的数在起点，那么需要从终点开始，否则如果数组本身有序，就会被交换掉
+            while (left < right && a[right] >= a[start]) {
                 right--;
             }
-            while (left < right && arr[left] <= arr[start]) {
+            while (left < right && a[left] <= a[start]) {
                 left++;
             }
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
+            swap(a, left, right);
         }
-        int temp = arr[right];
-        arr[right] = arr[start];
-        arr[start] = temp;
-        quickSort(arr, start, right - 1);
-        quickSort(arr, right + 1, end);
+        // 此处left 和right 相同, 换哪个都行
+        swap(a, right, start);
+        return left;
+    }
+
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
